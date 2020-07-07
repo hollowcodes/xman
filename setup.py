95aa41af25bc87af05ca0e98e069487e290f4dcf
hollowcodes <teddypeifer@gmail.com>
2020-07-07 12:32:30 +0200

    add gpl license

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ setuptools.setup(
     description="A simple libary to manage deep-learning experiments.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/hollowcodes/xman",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: Linux",
     ],
     python_requires='>=3.6',
 )
