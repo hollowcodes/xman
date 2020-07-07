42e629c2372010e219775f6de1da94d25ac73289
hollowcodes <teddypeifer@gmail.com>
2020-07-07 21:34:17 +0200

    overwritten directory structure

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
+
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="xman", # Replace with your own username
+    name="xman",
     version="0.0.1",
     author="Theodor Peifer",
     author_email="teddypeifer@gmail.com",
     description="A simple libary to manage deep-learning experiments.",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    py_modules=["xman"],
+    package_dir={" ": "src"},
     url="https://github.com/hollowcodes/xman",
     packages=setuptools.find_packages(),
     classifiers=[
